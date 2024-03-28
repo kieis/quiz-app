892da7ee7601bdb8cd8e9d519fb15bbf19b08bd7
Dirlan Ferreira <dirlan2014@yahoo.com.br>
2024-03-27 23:18:33 -0300

    feat: avoiding native return button on quiz to android

--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Alert, Text, View } from "react-native";
+import { Alert, BackHandler, Text, View } from "react-native";
 
 import { useNavigation, useRoute } from "@react-navigation/native";
 
@@ -7,6 +7,7 @@ import { styles } from "./styles";
 
 import { QUIZ } from "../../data/quiz";
 import { historyAdd } from "../../storage/quizHistoryStorage";
+import * as Haptics from "expo-haptics";
 
 import { Loading } from "../../components/Loading";
 import { Question } from "../../components/Question";
@@ -28,6 +29,7 @@ import { ProgressBar } from "../../components/ProgressBar";
 import { THEME } from "../../styles/theme";
 import { Gesture, GestureDetector } from "react-native-gesture-handler";
 import { OverlayFeedback } from "../../components/OverlayFeedback";
+import { Audio } from "expo-av";
 
 interface Params {
   id: string;
@@ -56,6 +58,17 @@ export function Quiz() {
 
   const route = useRoute();
   const { id } = route.params as Params;
+
+  async function playSound(isCorrect: boolean) {
+    const file = isCorrect
+      ? require("../../assets/correct.mp3")
+      : require("../../assets/wrong.mp3");
+
+    const { sound } = await Audio.Sound.createAsync(file, { shouldPlay: true });
+
+    await sound.setPositionAsync(0);
+    await sound.playAsync();
+  }
 
   function resetStatusReply() {
     setStatusReply(0);
@@ -97,12 +110,14 @@ export function Quiz() {
     }
 
     if (quiz.questions[currentQuestion].correct === alternativeSelected) {
+      await playSound(true);
       setStatusReply(1);
       setPoints((prevState) => prevState + 1);
       handleNextQuestion();
     } else {
+      await playSound(false);
       setStatusReply(2);
-      shakeAnimation();
+      await shakeAnimation();
     }
 
     setAlternativeSelected(null);
@@ -124,12 +139,14 @@ export function Quiz() {
     return true;
   }
 
-  function shakeAnimation() {
+  async function shakeAnimation() {
+    await Haptics.notificationAsync(Haptics.NotificationFeedbackType.Error);
+
     shake.value = withSequence(
       withTiming(3, { duration: 400, easing: Easing.bounce }),
       withTiming(0, undefined, (finished) => {
         "worklet";
-        if(finished) {
+        if (finished) {
           runOnJS(handleNextQuestion)();
           runOnJS(resetStatusReply)();
         }
@@ -217,13 +234,24 @@ export function Quiz() {
     }
   }, [points]);
 
+  useEffect(() => {
+    const backHandler = BackHandler.addEventListener(
+      "hardwareBackPress",
+      handleStop
+    );
+
+    return () => {
+      backHandler.remove();
+    };
+  }, []);
+
   if (isLoading) {
     return <Loading />;
   }
 
   return (
     <View style={styles.container}>
-      <OverlayFeedback status={statusReply}/>
+      <OverlayFeedback status={statusReply} />
       <Animated.View style={fixedProgressBarStyle}>
         <Text style={styles.title}>{quiz.title}</Text>
 
